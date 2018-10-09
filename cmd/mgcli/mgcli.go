0242bbf62c103f0842f9106476fba43199c3f80d
Ryan Kurte <ryankurte@gmail.com>
2018-10-09 18:31:13 +1300

    Added some commands, env variable setting

--- cmd/mgcli/mgcli.go
+++ cmd/mgcli/mgcli.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/jessevdk/go-flags"
@@ -9,16 +10,16 @@ import (
 )
 
 type Config struct {
-	Domain       string `short:"d" long:"domain" description:"Mailgun domain for sending" required:"true"`
-	APIKey       string `short:"k" long:"api-key" description:"Mailgun API key"  required:"true"`
-	PublicAPIKey string `short:"p" long:"public-api-key" description:"Mailgun public API key"  required:"true"`
+	Domain       string `short:"d" long:"domain" description:"Mailgun domain for sending" env:"MG_DOMAIN"`
+	APIKey       string `short:"k" long:"api-key" description:"Mailgun API key" env:"MG_API"`
+	PublicAPIKey string `short:"p" long:"public-api-key" description:"Mailgun public API key" env:"MG_PUBLIC_API"`
 
 	GetLists GetLists `command:"get-lists"`
 	AddList  AddList  `command:"add-list"`
 	Send     Send     `command:"send"`
+	Version  Version  `command:"version" description:"Show version and exit"`
 
 	Verbose bool `short:"v" long:"verbose" description:"Enable verbose logging"`
-	Version bool `long:"version" description:"Show version and exit"`
 }
 
 type Send struct {
@@ -27,35 +28,46 @@ type Send struct {
 	From    string `short:"f" long:"from" description:"Email from address"`
 }
 
-type GetLists struct{}
+type limitAndSkip struct {
+	Limit  int `long:"limit" description:"Maximum lists to return" default:"20"`
+	Offset int `long:"offset" description:"List index offset" default:"0"`
+}
+
+type GetLists struct {
+	limitAndSkip
+	Filter string `long:"filter" description:"List filter"`
+}
 
 type AddList struct {
 	Name string `long:"name" description:"List name" required:"true"`
 }
 
-var version string
+type Version struct{}
+
+var version string = "NOT SET"
 
 func main() {
 	c := Config{}
-	_, err := flags.Parse(&c)
+	p := flags.NewParser(&c, flags.Default)
+	_, err := p.Parse()
 	if err != nil {
-		log.Fatalf("%s", err)
-	}
-
-	if c.Version {
-		log.Printf("%s\n", version)
-		os.Exit(0)
+		log.Fatalf("Invalid arguments")
 	}
 
 	mg := mailgun.NewMailgun(c.Domain, c.APIKey, c.PublicAPIKey)
 
-	_, lists, err := mg.GetLists(100, 0, "")
-	if err != nil {
-		log.Fatalf("Error fetching list: %s", err)
-	}
-
-	if c.Verbose {
-		log.Infof("Found %d lists", len(lists))
+	switch p.Active.Name {
+	case "get-lists":
+		_, lists, err := mg.GetLists(c.GetLists.Limit, c.GetLists.Offset, c.GetLists.Filter)
+		if err != nil {
+			log.Fatalf("Error fetching list: %s", err)
+		}
+		fmt.Printf("Lists: %v", lists)
+	case "version":
+		fmt.Printf("%s\n", version)
+		os.Exit(0)
+	default:
+		log.Fatalf("Unsupported command")
 	}
 
 }
