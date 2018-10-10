936a09f36f8875f51dde22a51c4ce3e82dcb23c8
Ryan Kurte <ryankurte@gmail.com>
2018-10-10 15:46:52 +1300

    Added all the commands we hopefully need

--- cmd/mgcli/mgcli.go
+++ cmd/mgcli/mgcli.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/jessevdk/go-flags"
 	"github.com/mailgun/mailgun-go"
-	log "github.com/sirupsen/logrus"
 )
 
 type Config struct {
@@ -14,21 +14,28 @@ type Config struct {
 	APIKey       string `short:"k" long:"api-key" description:"Mailgun API key" env:"MG_APIKEY"`
 	PublicAPIKey string `short:"p" long:"public-api-key" description:"Mailgun public API key" env:"MG_PUBLIC_APIKEY"`
 
-	GetLists GetLists `command:"get-lists"`
-	AddList  AddList  `command:"add-list"`
-	Send     Send     `command:"send"`
-	Version  Version  `command:"version" description:"Show version and exit"`
+	GetLists  GetLists  `command:"get-lists" description:"Fetch existing mailing lists"`
+	AddList   AddList   `command:"create-list" description:"Create a new mailing list"`
+	AddMember AddMember `command:"add-member" description:"Add a member to a list"`
+	Send      Send      `command:"send" description:"Send an email to a list or address"`
 
-	Verbose bool `short:"v" long:"verbose" description:"Enable verbose logging"`
+	Version Version `command:"version" description:"Show version and exit"`
+	Verbose bool    `short:"v" long:"verbose" description:"Enable verbose logging"`
 }
 
 type Send struct {
 	Subject string   `short:"s" long:"subject" description:"Email subject" required:"true"`
-	Body    string   `short:"b" long:"body" description:"Email body" required:"true"`
+	Text    string   `short:"b" long:"body-text" description:"Email body text" required:"true"`
+	HTML    string   `long:"body-html" description:"Email body in html"`
 	From    string   `short:"f" long:"from" description:"Email from address" required:"true"`
 	To      []string `short:"t" long:"to" description:"Email to address(es)"`
 
+	CC  []string `long:"cc" description:"Addresses to copy to"`
+	BCC []string `long:"bcc" description:"Addresses to blind copy to"`
+
 	Headers map[string]string `short:"h" long:"headers" description:"Email headers"`
+	Test    bool              `long:"test" description:"Enable test mode"`
+	At      *time.Time        `long:"at" description:"Schedule sending at the provided time"`
 }
 
 type limitAndSkip struct {
@@ -47,6 +54,15 @@ type AddList struct {
 	Description string `long:"description" description:"List description"`
 }
 
+type AddMember struct {
+	List         string                 `short:"l" long:"list" description:"List name" required:"true"`
+	NoMerge      bool                   `long:"no-merge" description:"Disables updating existing members"`
+	Name         string                 `short:"n" long:"name" description:"Member name" required:"true"`
+	Address      string                 `short:"a" long:"address" description:"Member email address" required:"true"`
+	Unsubscribed bool                   `long:"unsubscribed" description:"Create member in unsubscribed state"`
+	Vars         map[string]interface{} `long:"var" description:"Member variables as K:V pairs"`
+}
+
 type Version struct{}
 
 var version string = "NOT SET"
@@ -56,7 +72,12 @@ func main() {
 	p := flags.NewParser(&c, flags.Default)
 	_, err := p.Parse()
 	if err != nil {
-		log.Fatalf("Invalid arguments")
+		//fmt.Printf("%s", err)
+		os.Exit(-1)
+	}
+
+	if c.Verbose {
+		fmt.Printf("Connecting to domain: '%s' with public API key: '%s'\n", c.Domain, c.APIKey)
 	}
 
 	mg := mailgun.NewMailgun(c.Domain, c.APIKey, c.PublicAPIKey)
@@ -65,28 +86,59 @@ func main() {
 	case "get-lists":
 		_, lists, err := mg.GetLists(c.GetLists.Limit, c.GetLists.Offset, c.GetLists.Filter)
 		if err != nil {
-			log.Fatalf("Error fetching list: %s", err)
+			fmt.Printf("Error fetching list: '%s'\n", err)
+			os.Exit(-2)
 		}
 		fmt.Printf("Lists: %+v", lists)
-	case "add-lists":
+	case "create-list":
 		list := mailgun.List{Address: c.AddList.Address, Name: c.AddList.Name, Description: c.AddList.Description}
 		list, err := mg.CreateList(list)
 		if err != nil {
-			log.Fatalf("Error fetching list: %s", err)
+			fmt.Printf("Error creating list: '%s'\n", err)
+			os.Exit(-3)
 		}
 		fmt.Printf("Created list")
+	case "add-member":
+		subscribed, merge := !c.AddMember.Unsubscribed, !c.AddMember.NoMerge
+		member := mailgun.Member{Address: c.AddMember.Address, Name: c.AddMember.Name, Subscribed: &subscribed, Vars: c.AddMember.Vars}
+		err := mg.CreateMember(merge, c.AddMember.List, member)
+		if err != nil {
+			fmt.Printf("Error creating member: '%s'\n", err)
+			os.Exit(-3)
+		}
+		fmt.Printf("Created member")
 	case "send":
-		m := mailgun.NewMessage(c.Send.From, c.Send.Subject, c.Send.Body, c.Send.To...)
+		m := mailgun.NewMessage(c.Send.From, c.Send.Subject, c.Send.Text, c.Send.To...)
+		if c.Send.HTML != "" {
+			m.SetHtml(c.Send.HTML)
+		}
+		if c.Send.At != nil {
+			m.SetDeliveryTime(*c.Send.At)
+		}
+		if c.Send.Test {
+			m.EnableTestMode()
+		}
+		for _, v := range c.Send.CC {
+			m.AddCC(v)
+		}
+		for _, v := range c.Send.BCC {
+			m.AddBCC(v)
+		}
+		for k, v := range c.Send.Headers {
+			m.AddHeader(k, v)
+		}
+
 		status, id, err := mg.Send(m)
 		if err != nil {
-			log.Fatalf("Error sending message: %s", err)
+			fmt.Printf("Error sending message: '%s'\n", err)
+			os.Exit(-4)
 		}
-		fmt.Printf("Message status: '%s' id: '%s'", status, id)
+		fmt.Printf("Message status: '%s' id: '%s'\n", status, id)
 	case "version":
 		fmt.Printf("%s\n", version)
-		os.Exit(0)
 	default:
-		log.Fatalf("Unsupported command")
+		fmt.Printf("Unsupported command\n")
+		os.Exit(-5)
 	}
 
 }
